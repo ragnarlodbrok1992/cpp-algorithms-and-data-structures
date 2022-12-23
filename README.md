f4bc8045224926c18db9bb4d0d8b50486f39d2e2
Maciej Oliwa <maciej.oliwa@nokia.com>
2022-12-23 15:31:58 +0100

    WIP

---
+++ README.md
@@ -0,0 +1,12 @@
+# C++ Algorithms And Data Structures
+## Why
+
+Just because. I want to learn and best way to learn is to do.
+Nobody can stop me!
+
+## Ideas
+
+## Tasks
+
+## Notes
+
--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
+void printNoArgs() {
     printf("Demo!\n");
+}
+
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        printNoArgs();
+        return 0;
+    } else {
+
+    }
+
     return 0;
 }
