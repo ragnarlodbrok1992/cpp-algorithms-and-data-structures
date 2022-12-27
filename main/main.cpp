78db2e109dce57436e101aadbf4b423a3dbe7bee
Maciej Oliwa <maciej.oliwa@nokia.com>
2022-12-27 12:38:48 +0100

    Added help

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <cstring>
 
 void printNoArgs() {
-    printf("Demo!\n");
+    // printf("Demo!\n");
+    printf("ERROR: No parameters provided. Use --help/-h to get list of features.\n");
+    printf("This is a demo app for Algorithms And Data Structures framework.\n");
+    printf("  Written by Maciej Oliwa (ragnar).\n");
+}
+
+void printHelp() {
+    printf("Usage: --help/-h -> Print this help message.\n");
 }
 
 int main(int argc, char** argv) {
@@ -10,6 +18,13 @@ int main(int argc, char** argv) {
         return 0;
     } else {
         // Get parameters
+        size_t arg = 0;
+        while (argv[++arg]) {
+            const char* param = argv[arg];
+            if (strcmp(param, "--help") == 0 || strcmp(param, "-h") == 0) {
+                printHelp();
+            }
+        }
     }
 
     return 0;
